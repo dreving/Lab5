0d800812b31af08e11a7e26d227a18c8cd3852c8
dreving <drevinwarrior@gmail.com>
2016-10-03 20:35:03 -0400

    Added controller_psi

--- controller.m
+++ controller.m
@@ -1,11 +1,11 @@
-classdef controller < handle
+classdef Controller < handle
     properties(Constant)
-      kxp = 1;
-      kxd = 1;
-      kyp = 1;
-      kyd = 0;
-      ktp = 1;
-      ktd = 0;
+      kxp = 1.5;%1.5;
+      kxd = .02;%.02;
+      kyp = 5;%1.5;
+      kyd = .02;%.02;
+      ktp = 5;
+      ktd = .01;
       maxSteer = pi/2;
       maxV = .5;
     end
@@ -13,19 +13,23 @@ classdef controller < handle
         r_r_prev = [0;0;0];
     end
     
-     methods(Static = true)
+     methods(Access = public)
          
-         function obj = controller()
+         function obj = Controller()
+            
          end
-         function u = feedback(obj,robPose,goalToWorld,dt)
+         
+         function [u_V, u_w] = feedback(obj,robPose,goalToWorld,dt)
              %TODO, Figure out how input parameters goalToWorld, actual to
              %World, dt, and previous error
-             disp(goalToWorld)
              r_r_p = robPose.aToB()*(goalToWorld.getPoseVec() - robPose.getPoseVec());
+             x_psi_z = [r_r_p(1) atan2(r_r_p(2),r_r_p(1)); r_r_p(3)];
              up = [obj.kxp 0 0; 0 obj.kyp obj.ktp]*r_r_p;
              deriv_error = (r_r_p - obj.r_r_prev)./dt;
              ud = [obj.kxd 0 0; 0 obj.kyd obj.ktd]*deriv_error;
              u = up+ud;
+             u_V = u(1);
+             u_w = u(2);
              obj.r_r_prev = r_r_p; %idk if this works
              
 %scaling code for steering to hard. Commented out to be implemented when added to feed forward             
@@ -34,9 +38,7 @@ classdef controller < handle
 %              end
          end
     end
-     methods(Access = public)
-         
-     end
+
      methods(Access = private)
          
     end
--- controller_psi.m
+++ controller_psi.m
@@ -1,11 +1,12 @@
-classdef controller < handle
+classdef Controller < handle
+    %rename to controller.m 
     properties(Constant)
-      kxp = 0;
-      kxd = 0;
-      kyp = 0;
-      kyd = 0;
-      ktp = 0;
-      ktd = 0;
+      kxp = 1.5;%1.5;
+      kxd = .02;%.02;
+      k_psi_p = 5;%1.5;
+      k_psi_d = .02;%.02;
+      ktp = 5;
+      ktd = .01;
       maxSteer = pi/2;
       maxV = .5;
     end
@@ -13,19 +14,24 @@ classdef controller < handle
         r_r_prev = [0;0;0];
     end
     
-     methods(Static = true)
+     methods(Access = public)
          
-         function obj = controller()
+         function obj = Controller()
+            
          end
-         function o = fe((obj,robPose,goalToWorld)
+         
+         function [u_V, u_w] = feedback(obj,robPose,goalToWorld,dt)
              %TODO, Figure out how input parameters goalToWorld, actual to
              %World, dt, and previous error
              r_r_p = robPose.aToB()*(goalToWorld.getPoseVec() - robPose.getPoseVec());
-             up = [obj.kxp 0 0; 0 obj.kyp obj.ktp]*r_r_p;
-             deriv_error = (r_r_p - obj.r_r_prev)./dt;
-             ud = [obj.kxd 0 0; 0 obj.kyd obj.ktd]*deriv_error;
+             x_psi_z = [r_r_p(1) atan2(r_r_p(2),r_r_p(1)); r_r_p(3)];
+             up = [obj.kxp 0 0; 0 obj.k_psi_p obj.ktp]* x_psi_z;
+             deriv_error = ( x_psi_z - obj.r_r_prev)./dt;
+             ud = [obj.kxd 0 0; 0 obj.k_psi_d obj.ktd]*deriv_error;
              u = up+ud;
-             obj.r_r_prev = r_r_p; %idk if this works
+             u_V = u(1);
+             u_w = u(2);
+             obj.r_r_prev =  x_psi_z; %idk if this works
              
 %scaling code for steering to hard. Commented out to be implemented when added to feed forward             
 %              if u(2) > controller.maxSteer
@@ -33,9 +39,7 @@ classdef controller < handle
 %              end
          end
     end
-     methods(Access = public)
-         
-     end
+
      methods(Access = private)
          
     end
